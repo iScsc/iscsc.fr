9d2a567c763ad1a11a7b367841a0fa60fc0b7251
atxr <alexandre.tullot@protonmail.com>
2022-08-21 15:31:15 +0200

    Create model for article and user

---
+++ server/models/article.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }, 
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
