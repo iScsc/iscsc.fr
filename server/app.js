eee7b37b5c20ca94a68d360945413f0b3e2cdb33
atxr <alexandre.tullot@protonmail.com>
2022-08-21 15:50:37 +0200

    Add signup route
    The route does not hash password for the moment

--- server/app.js
+++ server/app.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const mongoose = require('mongoose');
 const { createServer } = require('http');
 require('dotenv/config');
 
-const httpServer = createServer(app); 
+// models
+const Article = require('./models/article');
+const User = require('./models/user');
+
+const httpServer = createServer(app);
 
 const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  PORT
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    PORT
 } = process.env;
 
 app.get('/', (req, res) => {
     res.send('iscsc.fr is running');
+});
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    const { username, password, email } = req.body;
+    if (!username || !password) {
+        res.send(418);
+    }
+
+    const user = new User({
+        username,
+        hashedPassword: password,  // TODO: hash password
+        email
+    });
+    user.save()
+        .then((result) => {
+            res.send(`User ${result.username} successfully created`);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 mongoose
