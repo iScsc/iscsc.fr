97925aa7415d4ca922128e14aa4ffe669ffb227f
atxr <alexandre.tullot@protonmail.com>
2022-08-21 21:46:33 +0200

    Move user routes

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const { createServer } = require('http');
 const Article = require('./models/article');
-const User = require('./models/user');
-const bcrypt = require('bcrypt');
+const userRoutes = require('./routes/users');
 const httpServer = createServer(app);
 require('dotenv/config');
 
 app.use(express.json());
+app.use(userRoutes);
 
 const {
     DB_USER,
@@ -21,57 +21,6 @@ const {
 app.get('/', (req, res) => {
     res.send('iscsc.fr is running');
 });
-
-app.post('/signup', (req, res) => {
-    const { username, password, email } = req.body;
-    if (!username || !password) {
-        res.status(418).send("Missing username or password");
-    }
-
-    bcrypt.genSalt(SALT_ROUND, function (err, salt) {
-        bcrypt.hash(password, salt, function (err, hash) {
-            const user = new User({
-                username,
-                hashedPassword: hash,
-                email
-            });
-            user.save()
-                .then((result) => {
-                    res.send(`User ${result.username} successfully created`);
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-        });
-    });
-});
-
-app.get('/login', (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        res.status(418).send("Missing username or password");
-    }
-
-    // Get hashedPassword
-    User.findOne({ username }, (err, result) => {
-        if (result) {
-            const { hashedPassword } = result;
-
-            // compare
-            bcrypt.compare(password, hashedPassword, function (err, result) {
-                if (result) {
-                    res.send(`Logged as ${username} successfully`);
-                }
-                else {
-                    res.status(418).send(`Access denied: Invalid password for ${username}`);
-                }
-            })
-        }
-        else {
-            res.status(418).send(`User ${username} not found`);
-        }
-    })
-})
 
 mongoose
     .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@iscsc.a11re32.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
