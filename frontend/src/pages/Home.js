e3f79db8a24822da08dbeee64e08c4195e57e014
atxr <alexandre.tullot@protonmail.com>
2022-08-22 18:21:06 +0200

    Fetch and render all articles in /

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
-}
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch(`/api/articles`);
+      const json = await response.json();
+
+      if (response.ok) {
+        setArticles(json);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="articles">
+        {articles &&
+          articles.map((article) => (
+            <div key={article._id}>
+              <p>{article.title}</p>
+              <small>{article.author} - {article.createdAt}</small>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
