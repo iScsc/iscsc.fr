4c3f05def121f1606402e6b66fef43aab8407029
atxr <alexandre.tullot@protonmail.com>
2022-08-22 17:54:17 +0200

    Add simple home page with navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
