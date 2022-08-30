03e50532a9600b68eb233d95231651f8e3d3ffdf
atxr <alexandre.tullot@protonmail.com>
2022-08-30 14:33:46 +0200

    Add conditional routes based on AuthContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Blog from "./pages/blog/Blog";
 import Navbar from "./components/Navbar";
 import CreateArticle from "./pages/blog/CreateArticle";
@@ -7,6 +8,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
@@ -15,10 +17,19 @@ function App() {
           <Routes>
             <Route path="/" element={<Navigate to="/blog" />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/createArticle" element={<CreateArticle />} />
+            <Route
+              path="/blog/createArticle"
+              element={user ? <CreateArticle /> : <Navigate to="/login" />}
+            />
             <Route path="/blog/:id" element={<Article />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
+            <Route
+              path="login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
