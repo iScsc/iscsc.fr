163a02c238363e10b0b1613145a93b848576e845
atxr <alexandre.tullot@protonmail.com>
2022-08-30 15:05:29 +0200

    Reset the article context when loging out

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,14 +1,17 @@
 import { useAuthContext } from "./useAuthContext";
+import { useArticlesContext } from "./useArticlesContext";
 
 export const useLogout = () => {
-  const { dispatch } = useAuthContext();
+  const { dispatch: dispatchAuth } = useAuthContext();
+  const { dispatch: dispatchArticle } = useArticlesContext();
 
   const logout = () => {
     // sremove the saved user in the local storage
     localStorage.removeItem("user");
 
     // update auth context
-    dispatch({ type: "LOGOUT" });
+    dispatchAuth({ type: "LOGOUT" });
+    dispatchArticle({ type: "RESET" });
   };
 
   return { logout };
