0a9537140bb3191c6d9b0f263813a20b0895e71f
atxr <alexandre.tullot@protonmail.com>
2022-08-30 15:08:06 +0200

    Redirect to / when loging out

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     logout();
+    navigate("/");
   };
 
   return (
