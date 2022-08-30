5b9404b7f370b247ecf86289b3e492ae5f25e7f3
atxr <alexandre.tullot@protonmail.com>
2022-08-30 13:01:39 +0200

    Add requireAuth middleware to some routes

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const articleController = require('../controllers/articleController');
+const articleController = require("../controllers/articleController");
+const requireAuth = require("../middleware/requireAuth");
 
-router.get('/', articleController.getAll);
-router.get('/byAuthor', articleController.getByAuthor);
-router.get('/byAuthor/:author', articleController.getByAuthor);
-router.post('/create', articleController.create);
-router.get('/:id', articleController.getById);
-router.delete('/:id', articleController.deleteById);
+router.get("/", articleController.getAll);
+router.get("/byAuthor", articleController.getByAuthor);
+router.get("/byAuthor/:author", articleController.getByAuthor);
+router.post("/create", requireAuth, articleController.create);
+router.get("/:id", articleController.getById);
+router.delete("/:id", requireAuth, articleController.deleteById);
 
 module.exports = router;
