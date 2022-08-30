326eff5923038dd22fa5f5b006d45b148f231c34
atxr <alexandre.tullot@protonmail.com>
2022-08-30 11:17:17 +0200

    Add logout button to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
@@ -9,8 +15,15 @@ const Navbar = () => {
         </Link>
         <nav>
           <div>
-            <Link to="/login" className="nav-login">Log In</Link>
-            <Link to="/signup" className="nav-signup">Sign Up</Link>
+            <button onClick={handleClick} className="nav-logout">Log Out</button>
+          </div>
+          <div>
+            <Link to="/login" className="nav-login">
+              Log In
+            </Link>
+            <Link to="/signup" className="nav-signup">
+              Sign Up
+            </Link>
           </div>
         </nav>
       </div>
