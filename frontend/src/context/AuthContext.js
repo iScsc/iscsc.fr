a6bdc8e45898f2524256f06ead6defface71a2fb
atxr <alexandre.tullot@protonmail.com>
2022-08-30 14:24:12 +0200

    Remove log auth context

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -14,7 +14,6 @@ export const AuthContextProvider = ({ children }) => {
     }
   }, []);
 
-  console.log("AuthContext state: ", state);
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
