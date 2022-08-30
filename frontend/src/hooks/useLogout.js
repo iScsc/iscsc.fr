539b48cd041c82ba01df2ff2f6f62a3713194039
atxr <alexandre.tullot@protonmail.com>
2022-08-30 11:17:08 +0200

    Add logout hook

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+
+  const logout = () => {
+    // sremove the saved user in the local storage
+    localStorage.removeItem("user");
+
+    // update auth context
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return { logout };
+};
