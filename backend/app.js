7aba0b92b4179d2514076bf5dc658cd59fd878da
atxr <alexandre.tullot@protonmail.com>
2022-10-22 23:46:03 +0200

    Improve .env management
    Reduce the number of .env files
    Create only one file for frontend and backend .env files

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ const { createServer } = require("http");
 const userRoutes = require("./routes/users");
 const articleRoutes = require("./routes/articles");
 const httpServer = createServer(app);
-require("dotenv").config({ path: `./config/.env.${process.env.NODE_ENV}` });
+require("dotenv").config({ path: `./../config/.env.${process.env.NODE_ENV}` });
 
-const { DB_USER, DB_PASSWORD, DB_NAME, PORT, CLIENT_ORIGIN } = process.env;
+console.log('env', process.env)
+
+const { DB_USER, DB_PASSWORD, DB_NAME, NODE_LOCAL_PORT, CLIENT_ORIGIN } = process.env;
 
 app.use(cors({ origin: CLIENT_ORIGIN || "http://localhost:3000" }));
 app.use(express.json({ limit: "1MB" }));
@@ -24,8 +26,8 @@ mongoose
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@iscsc.a11re32.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    httpServer.listen(PORT || 5000, () => {
-      console.log(`Server listening: http://localhost:${PORT}`);
+    httpServer.listen(NODE_LOCAL_PORT || 8080, () => {
+      console.log(`Server listening: http://localhost:${NODE_LOCAL_PORT}`);
     });
   })
   .catch((err) => {
