64f4e5658f12cb4874088f3cefd5df6b55c7a1ce
ctmbl <mabileau.clement@gmail.com>
2022-11-12 01:40:02 -0500

    Move pushing code to bump.sh and remove postversion_push.sh

--- bump.sh
+++ bump.sh
@@ -38,7 +38,7 @@ fi
 # ----------- Variable definition -----------
 
 # Define needed variables
-CURRENT=$(npm pkg get version | sed 's/"//g')
+CURRENT_VERSION=$(npm pkg get version | sed 's/"//g')
 NEW_VERSION=$1
 BUMP_BRANCH="${NEW_VERSION}-version-bump"
 ISCSC_REMOTE=$(git remote -v | grep 'git@github.com:iScsc/iscsc.fr.git' | awk '{print $1}' | head --lines 1)
@@ -47,11 +47,11 @@ ISCSC_REMOTE=$(git remote -v | grep 'git@github.com:iScsc/iscsc.fr.git' | awk '{
 
 # Check if targeted version is > than current
 echo "[+] Current version is '${CURRENT}'"
-if [ ! $(semver "${NEW_VERSION}" -r ">$CURRENT") ]; then
-	echo "[-] '${NEW_VERSION}'<='$CURRENT', '${NEW_VERSION}' isn't accepted as new version."
+if [ ! $(semver "${NEW_VERSION}" -r ">${CURRENT_VERSION}") ]; then
+	echo "[-] '${NEW_VERSION}'<='${CURRENT_VERSION}', '${NEW_VERSION}' isn't accepted as new version."
 	exit 1
 fi
-echo "[+] '${NEW_VERSION}'>'$CURRENT', '${NEW_VERSION}' is accepted as new version."
+echo "[+] '${NEW_VERSION}'>'${CURRENT_VERSION}', '${NEW_VERSION}' is accepted as new version."
 
 # ----------- Git setup -----------
 
@@ -79,3 +79,7 @@ echo '[+] Commiting `frontend` and `backend` bump'
 
 echo '[+] Bumping `root`'
 [ -z "$DRY_RUN" ] && npm version "${NEW_VERSION}" -m "Bump to version %s"
+
+echo '[+] Pushing branch and new version tag'
+echo "[~] pushing to \`${ISCSC_REMOTE}\` please type your passphrase/password if required:"
+[ -z "$DRY_RUN" ] && git push ${ISCSC_REMOTE} ${BUMP_BRANCH} v${CURRENT_VERSION} || echo "[-] push failed, you can push with \`git push ${ISCSC_REMOTE} ${BUMP_BRANCH} v${CURRENT_VERSION}\`"
