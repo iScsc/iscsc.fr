4e972028b8036c646f220823fbf003a6ec23e5ae
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-15 18:36:08 +0100

    Fetch an article if articles are null

--- frontend/src/pages/blog/Article.js
+++ frontend/src/pages/blog/Article.js
@@ -4,9 +4,28 @@ import { useArticlesContext } from "../../hooks/useArticlesContext";
 
 const Article = () => {
   let { id } = useParams();
-  const { articles } = useArticlesContext();
-  const article = articles.filter((a) => a._id === id)[0];
-  return <ArticleView article={article} />;
+  const { articles, dispatch } = useArticlesContext();
+
+  const fetchArticle = async () => {
+      const response = await fetch(`/api/articles/${id}`);  // fetch the article (only this one) from the database
+      if (response.ok) {
+        const article = await response.json();
+        dispatch({ type: "ADD", payload: article });       // if it exists, add it to the ArticlesContext
+        return article;
+      }
+      else {
+        return null;
+      } 
+  };
+  
+  try {
+    const article = articles.filter((a) => a._id === id)[0];
+    return <ArticleView article={article} />;
+  } catch (error) { //if articles are null, articles.filter produces an error
+    const article = fetchArticle();
+    return <ArticleView article={article} />;
+  }
+
 };
 
 export default Article;
