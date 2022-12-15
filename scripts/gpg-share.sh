e58b314144a726077f26667baab95a53af4eeebe
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2022-12-16 00:02:41 +0100

    FEATURE: add flag support to the `gpg-share` script (#48)
    * add basic flag support and put the code in main
    
    This commit will NOT work as check_users() needs the users array to
    be defined from outside the function and before its definition...
    I'll work on that in next commits!
    
    * give real arguments to the functions and remove the exit 1
    
    * pass the users array as argument to functions
    
    This commit also reactivates the script as before.
    
    * give the file and the users with more flags
    
    * show the usage when the script is not used properly
    
    * do not use `echo -n` to get the user when iterating
    
    As the array of users is now properly built and properly passed as an
    argument to `check_users` and `encrypt`, there is no need to remove
    extra newlines with `echo -n` from inside the loops iterating over the
    array of users.
    
    * update the usage string
    
    * update the help message
    
    * fix the error log messages with mentions to the new flags
    
    * require an argument after the `--file` flag
    
    * do not allow the user to set the file multiple times
    
    The script now checks if the `file` variable has already been set
    by a `-f` and exits if it's the case.
    `file` has to be set to `""` to avoid an external value to change
    the behaviour of the script, e.g. setting `file` as en environment
    variable before running the script: behaviour untested.
    
    * use `--with-colons` with `gpg` as good practive
    
    A single gpg key has the following form:
    ```bash
    > gpg --list-keys --with-colons user
    tru::x:x:x:x:x:x
    pub:x:x:x:<small key>:x:x::x:::x:::::x:::x:
    fpr:::::::::<pub id>:
    uid:x::::x::<uid>::<name>::::::::::x:
    sub:x:x:x:<small key>:x:x:::::x:::::x::
    fpr:::::::::<sub id>:
    ```
    
    There is a single "^uid", so we can still count the number of
    ids matching the input pattern with a simple `grep "^uid" | wc -l`.
    
    Then, when there is a single user matching the input, we want the
    `<pub id>`, i.e. the first "^fpr" after the single "^pub".
    And the `<pub id>` appear to be the 10th column.
    
    So the full id-extraction command is
    ```bash
    gpg --quiet --list-keys --with-colons "$user"
    | grep "^pub" -A1  # get the pub and the following line
    | grep "^fpr"  # get the fpr line
    | awk -F ":" '{print $10}'  # get the 10th column
    ```
    
    The script works just as before.

--- scripts/gpg-share.sh
+++ scripts/gpg-share.sh
@@ -31,6 +31,8 @@ DEPENDENCIES=(
   tar
 )
 
+USAGE="gpg-share [-h] -f FILE {-u USER} {-U USER_FILE}"
+
 
 # TODO: documentation
 check_dependencies () {
@@ -61,20 +63,11 @@ check_pwd () {
 }
 
 
-check_dependencies
-check_pwd
-
-# get the version
-VERSION=$(npm pkg get version | sed 's/"//g')
-
-# build the archive name
-ARCHIVE="keys-${VERSION}.tar"
-
-
 # TODO: documentation
 check_file () {
   [ -z "$1" ] && {
-    log_error "Please give a file to share as the first argument"
+    log_error 'Please give a file to share with the `-f` flag'
+    usage
     exit 1
   }
 
@@ -85,34 +78,24 @@ check_file () {
 }
 
 
-file="$1"; shift 1
-check_file "$file"
-
-if [ -n "$USER_FILE" ]; then
-  check_file "$USER_FILE"
-  readarray users < "$USER_FILE"
-else
-  users=($@)
-fi
-
-
 # TODO: documentation
 check_users() {
+  users=("$@")
   [ "${#users[@]}" -eq 0 ] && {
-    log_error "Please give at least one gpg id after the first argument"
+    log_error 'Please give at least one gpg id with the `-u` and `-U` flags'
+    usage
     exit 1
   }
 
   # check if each of the "user" is in the keyring
   invalid=0
-  for _user in "${users[@]}";
+  for user in "${users[@]}";
   do
-      user=$(echo -n "$_user")
-      nb_ids=$(gpg --quiet --list-keys "$user" 2> /dev/null | grep "^uid" | wc -l)
+      nb_ids=$(gpg --quiet --list-keys --with-colons "$user" 2> /dev/null | grep "^uid" | wc -l)
       case "$nb_ids" in
         0 ) log_warning "'$user' not found in the keyring..."; invalid=1 ;;
         1 )
-          id=$(gpg --quiet --list-keys "$user" | grep uid -B1 | head -n 1 | awk '{print $1}');
+          id=$(gpg --quiet --list-keys --with-colons "$user" | grep "^pub" -A1 | grep "^fpr" | awk -F ":" '{print $10}');
           log_success "key $id found for '$user'"
           ;;
         * ) log_warning "too many ids found for '$user' in keyring ($nb_ids)"; invalid=1 ;;
@@ -125,36 +108,111 @@ check_users() {
 }
 
 
-check_users
-
-DUMP_DIR=$(mktemp -u "/tmp/gpg-share-XXXXXX")
-mkdir -p "$DUMP_DIR"
-
-
 # TODO: documentation
 encrypt () {
-  for _user in "${users[@]}";
+  file="$1"
+  directory="$2"
+  shift 2
+  users=("$@")
+  for user in "${users[@]}";
   do
-      user=$(echo -n "$_user")
-      log_info "encrypting '$file' for '$user' inside '$DUMP_DIR'..."
-      output=$(echo "$DUMP_DIR/$file.$user.asc" | sed 's/\s\+/-/g')
+      log_info "encrypting '$file' for '$user' inside '$directory'..."
+      output=$(echo "$directory/$file.$user.asc" | sed 's/\s\+/-/g')
       gpg --verbose --recipient "$user" --encrypt --armor --output "$output" "$file"
   done
-  log_success "all encrypted file have been saved inside '$DUMP_DIR'"
+  log_success "all encrypted file have been saved inside '$directory'"
 }
 
 
 # TODO: documentation
 archive () {
+  directory="$1"
+  name="$2"
   log_info "storing files in archive"
   # subshell here to avoid archiving the paths to the .asc files
   (
-    cd "$DUMP_DIR"
-    tar cvf "$ARCHIVE" $(find . -type f)
+    cd "$directory"
+    tar cvf "$name" $(find . -type f)
   )
-  cp "$DUMP_DIR/$ARCHIVE" .
+  cp "$directory/$name" .
 }
 
 
-encrypt
-archive
+# parse the arguments.
+OPTIONS=$(getopt -o hu:U:f: --long help,user:,users:,file: -n 'gpg-share' -- "$@")
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$OPTIONS"
+
+
+usage () {
+  echo "Usage: $USAGE"
+  echo "Type -h or --help for the full help."
+  exit 0
+}
+
+
+help () {
+  echo "gpg-share:"
+  echo "     a script to share a secret file with others using pgp public keys."
+  echo "     it should guide you when you make mistakes, e.g. providing a non-existing file."
+  echo ""
+  echo "Usage:"
+  echo "     $USAGE"
+  echo ""
+  echo "Options:"
+  echo "     -h/--help               shows this help."
+  echo "     -f/--file               the secret file to share."
+  echo "     -u/--user               a single user id in the keyring."
+  echo "                               can be used multiple times."
+  echo "                               supports multi-word ids inside quotes, e.g. \"John Smith\"."
+  echo "     -U/--users              the path to a file containing pgp ids."
+  echo "                               should contain exactly one valid pgp id per line."
+  echo "                               can be used multiple times."
+  exit 0
+}
+
+
+main () {
+  file=""
+  users=()
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h | --help ) help ;;
+      -f | --file )
+        [ -n "$file" ] && {
+          log_error "cannot set file multiple times with -f"
+          exit 1
+        }
+        file="$2"; shift 2;;
+      -u | --user ) users+=("$2"); shift 2;;
+      -U | --users ) 
+        check_file "$2"
+        readarray new_users < "$2"
+        shift 2
+        for new_user in "${new_users[@]}"; do
+          users+=("$(echo -n "$new_user")")
+        done;;
+      -- ) shift; break ;;
+      * ) break ;;
+    esac
+  done
+
+  check_dependencies
+  check_pwd
+
+  check_file "$file"
+
+  check_users "${users[@]}"
+
+  archive_directory=$(mktemp -u "/tmp/gpg-share-XXXXXX")
+  mkdir -p "$archive_directory"
+
+  encrypt "$file" "$archive_directory" "${users[@]}"
+
+  repo_version=$(npm pkg get version | sed 's/"//g')
+  archive_name="keys-${repo_version}.tar"
+  archive "$archive_directory" "$archive_name"
+}
+
+
+main "$@"
