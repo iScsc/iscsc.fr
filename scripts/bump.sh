5da6c41300b710c25c6479f383404737c9be1cf2
ClementMabileau <79016298+ctmbl@users.noreply.github.com>
2023-03-17 13:40:54 -0400

    Add subcommands to bump script (#71)
    * Wrap everything in functions
    
    * Correct typo
    
    * Add arguments handling
    
    * Update check_args fucntion and add usage
    
    * Add help
    
    * Update check_version_semantics
    
    * Add check_version_and_tag_consistency
    
    * Add break_down_version
    
    * Add version bump functions
    
    * Fix arguments handling bug
    
    * Fix tag version check bug
    
    * Change the USAGE format
    
    * Move argument parsing to the top of the main function
    
    * Fix wrong array expansion passed as multiples args to logs func
    
    * Fix broken NEW_VERSION defintion due to empty CURRENT_VERSION
    
    * Format sections comment
    
    * Change remote check failure from error to warning and improve log
    
    Add a PUSH variable set to 'n' if the ssh remote is not detected
    
    * Revert "Change remote check failure from error to warning and improve log"
    
    This reverts commit 9fae4dd6e630a5bdb8c04931d9ffa9c693d96f8e.
    
    * Change ISCSC_REMOTE to ISCSC_REMOTE_NAME
    
    * Allow HTTPS iScsc remote but log a warning, exit if remote not found
    
    * Send grep output to /dev/null to keep only the remote URL in get_remote
    
    * Change local iscsc_remote var to iscsc_remote_name
    
    * Bring back previously removed exit if checkout and switch fail
    
    * Add --detach to git checkout call to remove annoying git advice
    
    * Add --ignore-case to remote greps to match atxr remotes
    
    * Add doctstring to break_down_version
    
    * Revert remote grep work on ssh/https (see commit body)
    
    Revert "Add --ignore-case to remote greps to match atxr remotes"
    This reverts commit 4c014d2dbbe28e5e6a4e2abd0083732405a080cc.
    Revert "Change local iscsc_remote var to iscsc_remote_name"
    This reverts commit 2bbf87792b1948591a2c64a807bb9336e07828b3.
    Revert "Send grep output to /dev/null to keep only the remote URL in get_remote"
    This reverts commit ac05499542d8d9f88e449dfd35bbd6c7fd3bf16b.
    Revert "Allow HTTPS iScsc remote but log a warning, exit if remote not found"
    This reverts commit 026aef426b8deb363484038022e6e5fb7890b424.
    Revert "Change ISCSC_REMOTE to ISCSC_REMOTE_NAME"
    This reverts commit bb610d4210b024151aca5fb35f3e471187d0317d.
    
    * Improve remote grepping to be flexible and clearer (see commit body)
    
    After reverting last work on remote grepping I:
     - brought back get_remote function but comment it
     - use 'iscsc/iscsc.fr' and ignore case when pattern matching the remote
    	- this allow HTTPS remotes
    	- this imporve reliability against edge cases
     - log warning in any case to warn that only first remote is kept
     - keep only first remote found
     - log warning if the remote found is an HTTPS

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -8,6 +8,8 @@ DEPENDENCIES=(
 
 # Variables
 NB_ARGS="$#"
+USAGE="bump.sh [-h | --help] [-p | --patch] [-m | --minor] [-M | --major]"
+REMOTE_URL_PATTERN="iscsc/iscsc.fr"
 
 # ------------------------------- Tool functions -------------------------------
 
@@ -21,9 +23,16 @@ log_warning () { log_any "33" "~" "$1"; }
 log_hint    () { log_any "34" "?" "$1"; }
 log_info    () { log_any "35" "i" "$1"; }
 
+break_down_version () {
+	# # Exemple
+	# V=1.2.3
+	# echo ${V//./ } # 1 2 3
+    echo ${1//./ }
+}
+
 # -------------------------------- Basic Checks --------------------------------
 
-# check that current directory is repo root
+# Check that current directory is repo root
 check_pwd () {
 	root="$(dirname $(realpath $(dirname "$0")))"
 	[ "$root" != "$(pwd)" ] && {
@@ -40,9 +49,7 @@ check_arg () {
 	local nb_args="$1"
 	if [ "${nb_args}" -ne "1" ]; then
 		log_error "Exactly one argument is needed, got ${nb_args}."
-		log_hint 'Example: `./bump.sh 0.2.6`'
-		log_hint "see https://github.com/iScsc/iscsc.fr/wiki/Version-bump-procedure#automatic-version-bump for full documentation"
-		exit 1
+		usage
 	fi
 	[ -n "$DRY_RUN" ] && log_ok "Only argument has been given OK"
 }
@@ -51,11 +58,11 @@ check_arg () {
 check_dependencies () {
 	for package in ${DEPENDENCIES[@]}; do
 		if [ ! $(which $package) ]; then
-			log_error "All of '${DEPENDENCIES[@]}' are needed to bump version"
+			log_error "All of '$(echo ${DEPENDENCIES[@]})' are needed to bump version"
 			exit 1
 		fi
 	done
-	[ -n "$DRY_RUN" ] && log_ok "Dependencies: '${DEPENDENCIES[@]}' are installed on the system OK"
+	[ -n "$DRY_RUN" ] && log_ok "Dependencies: '$(echo ${DEPENDENCIES[@]})' are installed on the system OK"
 }
 
 # Check that git working directory is clean...
@@ -67,30 +74,36 @@ check_clean_git_working_dir () {
 	[ -n "$DRY_RUN" ] && log_ok "git working directory is clean OK"
 }
 
-# Run all basic checks
-check_pwd
-check_arg "${NB_ARGS}"
-check_dependencies
-check_clean_git_working_dir
-
-# ---------------------------- Variables definition ----------------------------
-
-# Define needed variables
-NEW_VERSION="$1"; shift 1;
-CURRENT_VERSION=$(npm pkg get version | sed 's/"//g')
-BUMP_BRANCH="${NEW_VERSION}-version-bump"
-ISCSC_REMOTE=$(git remote -v | grep 'git@github.com:iScsc/iscsc.fr.git' | awk '{print $1}' | head --lines 1)
-
 # ------------------------------ Advanced checks -------------------------------
 
 # Check that supplied version is semantically correct
 check_version_semantics () {
 	local version="$1"
+	if [ -z "${version}" ]; then
+		log_error "You provided a wrong argument, version can't be computed"
+		usage
+		exit 1
+	fi
 	if [ "${version}" != "$(semver ${version})" ]; then
 		log_error "${version} is not a valid version number according to semver"
 		exit 1
 	fi
 	[ -n "$DRY_RUN" ] && log_ok "Provided version '${version}' is semantically correct OK"
+}
+
+# Check that last tag and current version are consistent
+check_version_and_tag_consistency () {
+	local json_version="$1"
+	tagged_commit=$(git rev-list --tags --max-count=1)
+	tag_version=$(git describe --tags $tagged_commit)
+	# remove the 'v' before the version
+	tag_version=$(semver $tag_version)
+
+	if [ "$json_version" != "$tag_version" ]; then
+		log_error "tag '$tag_version' is not consistent with project version '${json_version}' !!!"
+		exit 1
+	fi
+	[ -n "$DRY_RUN" ] && log_ok "tag and project version are consistent OK"
 }
 
 # Check that targeted version is > than current
@@ -109,49 +122,174 @@ check_iscsc_remote () {
 	if [ -z "$remote" ]; then
 		log_error "'iScsc/iscsc.fr' remote is not in remote list"
 		exit 1
+	elif [ ! -z "$(git remote get-url ${remote} | grep 'https' --ignore-case)" ]; then
+		log_warning "Found an HTTPS remote, pushing may not work properly"
 	fi
 	log_ok "'iScsc/iscsc.fr' is in remote list as '$remote' OK"
 }
 
-# Run all advanced checks
-check_version_semantics "${NEW_VERSION}"
-check_version_greater
-check_iscsc_remote "${ISCSC_REMOTE}"
-
-log_info "'${NEW_VERSION}'>'${CURRENT_VERSION}', '${NEW_VERSION}' is accepted as new version."
 # --------------------------------- Git setup ----------------------------------
 
-# ...and checkout on main to create a version bump branch
-# (working dir is empty check passed)
-log_info "Checkout on ${ISCSC_REMOTE}/main"
-[ -z "$DRY_RUN" ] && git checkout ${ISCSC_REMOTE}/main
-log_info "switching to ${BUMP_BRANCH}"
-[ -z "$DRY_RUN" ] && git switch -c ${BUMP_BRANCH}
+get_remote () {
+	# List git remotes, grep for the iscsc/iscsc.fr one and filter for push remotes
+	# In case push and fetch URL are different, further checkout may fail
+	local remotes=$(git remote -v | grep "${REMOTE_URL_PATTERN}" --ignore-case | grep "push")
+
+	# Can't log_warning in here because get_remote is called as a command
+	# substitution: all stdout will end up in ISCSC_REMOTE (see main function)
+
+	# Keep only the first remote found and grep first column (its name)
+	echo "$remotes" | head --lines 1 | awk '{print $1}'
+}
+
+git_setup () {
+	local iscsc_remote="$1"
+	local bump_branch="$2"
+	# ...and checkout on main to create a version bump branch
+	# (working dir is empty check passed)
+	log_info "Checkout on ${iscsc_remote}/main"
+	[ -z "$DRY_RUN" ] && { git checkout ${iscsc_remote}/main --detach || exit 1; }
+	log_info "switching to ${bump_branch}"
+	[ -z "$DRY_RUN" ] && { git switch -c ${bump_branch} || exit 1; }
+}
 
 # -------------------------------- Version Bump --------------------------------
 # ------------------------- Bump frontend and backend --------------------------
 
-log_info 'Bumping `frontend`'
-(
-	cd frontend
-	[ -z "$DRY_RUN" ] && npm version "${NEW_VERSION}" --no-git-tag-version
-)
-log_info 'Bumping `backend`'
-(
-	cd backend
-	[ -z "$DRY_RUN" ] && npm version "${NEW_VERSION}" --no-git-tag-version
-)
+bump_modules () {
+	local new_version="$1"
+	log_info 'Bumping `frontend`'
+	(
+		cd frontend
+		[ -z "$DRY_RUN" ] && npm version "${new_version}" --no-git-tag-version
+	)
+	log_info 'Bumping `backend`'
+	(
+		cd backend
+		[ -z "$DRY_RUN" ] && npm version "${new_version}" --no-git-tag-version
+	)
+}
 
 # ----------------------------- Bump root and push -----------------------------
 
-log_info 'Bumping `root`'
-[ -z "$DRY_RUN" ] && { npm version "${NEW_VERSION}" --no-git-tag-version || exit 1; }
-[ -z "$DRY_RUN" ] && { git commit -m "Bump to version ${NEW_VERSION}" || exit 1; }
+bump_root () {
+	local new_version="$1"
+	log_info 'Bumping `root`'
+	[ -z "$DRY_RUN" ] && { npm version "${new_version}" --no-git-tag-version || exit 1; }
+	[ -z "$DRY_RUN" ] && { git commit -m "Bump to version ${new_version}" || exit 1; }
+}
 
-log_info 'Pushing branch and bump commit'
-log_warning "pushing to \`${ISCSC_REMOTE}\` please type your passphrase/password if required:"
-PUSH_COMMAND="git push ${ISCSC_REMOTE} ${BUMP_BRANCH} v${CURRENT_VERSION}"
-[ -z "$DRY_RUN" ] && { $PUSH_COMMAND || log_error "push failed, you can push with \`${PUSH_COMMAND}\`"; }
+# ------------------------------- Help and usage -------------------------------
 
-log_hint '`npm install` has been run during the bump, you MUST review the changes during PR review to ensure package.json and package-lock.json where compatible!!!'
+usage () {
+	echo "Usage: $USAGE"
+	echo "Type -h or --help for the full help."
+	echo "See https://github.com/iScsc/iscsc.fr/wiki/Version-bump-procedure#automatic-version-bump for full documentation"
+	exit 0
+}
 
+help () {
+	echo "bump.sh:"
+	echo "     a script to easily bump the version of the iScsc website project."
+	echo "     it should guide you when you make mistakes, e.g. providing less or more than 1 argument."
+	echo ""
+	echo "Usage:"
+	echo "     $USAGE"
+	echo ""
+	echo "Options:"
+	echo "     -h/--help                shows this help."
+	echo "     -p/--patch               bump to patch version (1.2.3 --> 1.2.4)."
+	echo "     -m/--minor               bump to minor version (1.2.3 --> 1.3.0)."
+	echo "     -M/--major               bump to major version (1.2.3 --> 2.0.0)."
+	exit 0
+}
+
+# --------------------------- Version Bump Functions ---------------------------
+
+bump_major () {
+    bits=($(break_down_version $1))
+    major=${bits[0]}
+
+    echo "$((major+1)).0.0"
+}
+
+
+bump_minor () {
+    bits=($(break_down_version $1))
+    major=${bits[0]}
+    minor=${bits[1]}
+
+    echo "$major.$((minor+1)).0"
+}
+
+
+bump_patch () {
+    bits=($(break_down_version $1))
+    major=${bits[0]}
+    minor=${bits[1]}
+    patch=${bits[2]}
+
+    echo "$major.$minor.$((patch+1))"
+}
+
+# -------------------------------- Main Section --------------------------------
+
+main () {
+	COMMAND=""
+	# Extract new version from arguments
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-h | --help ) help ;;
+			-p | --patch ) COMMAND="patch"; shift 1;;
+			-m | --minor ) COMMAND="minor"; shift 1;;
+			-M | --major ) COMMAND="major"; shift 1;;
+			-- ) shift; break ;;
+			* ) break ;;
+		esac
+	done
+
+	# Run all basic checks
+	check_pwd
+	check_arg "${NB_ARGS}"
+	check_dependencies
+	check_clean_git_working_dir
+
+	# Define version variables
+	CURRENT_VERSION=$(npm pkg get version | sed 's/"//g')
+	NEW_VERSION=$(bump_${COMMAND} ${CURRENT_VERSION})
+
+	# Define git variables
+	BUMP_BRANCH="${NEW_VERSION}-version-bump"
+	log_warning "If multiple remotes are found for '${REMOTE_URL_PATTERN}' only first is kept"
+	ISCSC_REMOTE=$(get_remote)
+
+	# Run all advanced checks
+	check_version_semantics "${NEW_VERSION}"
+	check_version_and_tag_consistency "${CURRENT_VERSION}"
+	check_version_greater
+	check_iscsc_remote "${ISCSC_REMOTE}"
+
+	log_info "'${NEW_VERSION}'>'${CURRENT_VERSION}', '${NEW_VERSION}' is accepted as new version."
+
+	# Setup git branch
+	git_setup ${ISCSC_REMOTE} ${BUMP_BRANCH}
+
+	# Bump
+	bump_modules ${NEW_VERSION}
+	bump_root ${NEW_VERSION}
+
+	# Try to push bump refs
+	log_info 'Pushing branch and bump commit'
+	log_warning "pushing to \`${ISCSC_REMOTE}\` please type your passphrase/password if required:"
+	PUSH_COMMAND="git push ${ISCSC_REMOTE} ${BUMP_BRANCH} v${CURRENT_VERSION}"
+	[ -z "$DRY_RUN" ] && { $PUSH_COMMAND || log_error "push failed, you can push with \`${PUSH_COMMAND}\`"; }
+
+	log_hint '`npm install` has been run during the bump, you MUST review the changes during PR review to ensure package.json and package-lock.json where compatible!!!'
+}
+
+# parse the arguments.
+OPTIONS=$(getopt -o hpmM --long help,patch,minor,major -n 'bump' -- "$@")
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$OPTIONS"
+
+main "$@"
