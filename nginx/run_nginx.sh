fbf76472f747603bc7d510889c0b5dd0d16710d4
ctmbl <mabileau.clement@gmail.com>
2023-03-30 15:04:29 +0200

    Separate dev and prod mode for the nginx template
    created 2 separate template conf and pass an env var MODE
    from .env to docker to run.sh for the nginx config

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -3,5 +3,5 @@
 # To avoid injection, use ${DOLLAR}DONT_INJECT_ME
 # Start nginx when the config file is genereated
 export DOLLAR='$'
-envsubst < nginx.conf.template > /etc/nginx/nginx.conf
+envsubst < nginx.conf.${MODE}.template > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
