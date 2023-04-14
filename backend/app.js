9e4cd0f9d766e4b0c2054e859b0305f9939cda9b
ClementMabileau <mabileau.clement@gmail.com>
2023-04-15 01:20:55 +0200

    Refactor backend docker environment towards a python flask backend (#88)
    * Add requirements.txt file for future python backend
    
    * Refactor Dockerfile to run a flask backend
    
    * Rename NODE_PORT to BACKEND_PORT
    
    * Rename node-app to flask-app
    
    * Rename BACKEND_PORT to FLASK_RUN_PORT
    
    * Merge with main: FIX: remove the `with` block under `actions/checkout@v3` in `prettier` (#91)
    
    * Run prettier
    
    ---------
    
    Co-authored-by: Antoine Stevan <44101798+amtoine@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const articleRoutes = require('./routes/articles')
 const httpServer = createServer(app)
 require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}` })
 
-const { DB_USER, DB_PASSWORD, DB_NAME, NODE_PORT, CLIENT_ORIGIN } = process.env
+const { DB_USER, DB_PASSWORD, DB_NAME, FLASK_RUN_PORT, CLIENT_ORIGIN } =
+  process.env
 
 app.use(cors({ origin: CLIENT_ORIGIN || 'http://localhost:3000' }))
 app.use(express.json({ limit: '1MB' }))
@@ -24,8 +25,8 @@ mongoose
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@iscsc.a11re32.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    httpServer.listen(NODE_PORT || 3001, () => {
-      console.log(`Server listening: http://localhost:${NODE_PORT}`)
+    httpServer.listen(FLASK_RUN_PORT || 3001, () => {
+      console.log(`Server listening: http://localhost:${FLASK_RUN_PORT}`)
     })
   })
   .catch(err => {
