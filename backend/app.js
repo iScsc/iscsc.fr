5764dba1973a0d6205311e5c34ee300fb9011d50
Alexandre Tullot <alexandre.tullot@protonmail.com>
2023-04-16 12:16:46 +0200

    Migrate database to localhost (#92)
    This PR removes the *hardcoded* values of the remote atlas database.
    It introduces a new local database deployment in the docker-compose
    workflow.
    The database is secured thanks to docker networking.
    
    I tested the whole migration :heavy_check_mark: 
    
    :warning: This PR doesn't migrate the current atlas db to the new local
    one. This action must be done "by hand" when the website will be bumped.

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const articleRoutes = require('./routes/articles')
 const httpServer = createServer(app)
 require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}` })
 
-const { DB_USER, DB_PASSWORD, DB_NAME, NODE_PORT, CLIENT_ORIGIN } = process.env
+const { DB_PORT, DB_NAME, NODE_PORT, CLIENT_ORIGIN } = process.env
 
 app.use(cors({ origin: CLIENT_ORIGIN || 'http://localhost:3000' }))
 app.use(express.json({ limit: '1MB' }))
@@ -19,10 +19,9 @@ app.get('/', (req, res) => {
   res.send('iscsc.fr is running')
 })
 
+uri = `mongodb://mongodb:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`
 mongoose
-  .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@iscsc.a11re32.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-  )
+  .connect(uri)
   .then(() => {
     httpServer.listen(NODE_PORT || 3001, () => {
       console.log(`Server listening: http://localhost:${NODE_PORT}`)
