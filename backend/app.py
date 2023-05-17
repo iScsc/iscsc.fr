592bbe5f20b549417b545f955675b921ec5166a8
ctmbl <mabileau.clement@gmail.com>
2023-05-17 10:34:02 +0200

    Fixes: working basic routes articles and articles/<id>

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 dummy_article = {
     'author':'ctmbl',
     'title':'My dummy article',
-    'body':'This is a dummy article hardcoded to build the backend'
+    'body':'This is a dummy article hardcoded to build the backend',
+    '_id':"1"
+    }
+
+dummy_article_2 = {
+    'author':'ctmbl',
+    'title':'My dummy article 2',
+    'body':'This is a dummy article 2',
+    '_id':"2"
     }
 
 @app.route('/')
@@ -14,8 +22,8 @@ def root():
 
 @app.route('/api/articles', methods=['GET'])
 def fetch_all_articles():
-    return jsonify([dummy_article, dummy_article])
+    return jsonify([dummy_article, dummy_article_2])
 
-@app.route('/api/article/<id>', methods=['GET'])
+@app.route('/api/articles/<id>', methods=['GET'])
 def fetch_article(id):
     return jsonify(dummy_article)
