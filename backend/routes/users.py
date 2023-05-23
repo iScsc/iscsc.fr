add2e78990f91067ba1cb5157c7a8feddd0ea6fc
atxr <alexandre.tullot@protonmail.com>
2023-05-23 22:11:43 +0200

    Add missing fixme

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from controllers.userController import *
 
-users = Blueprint("users", "backend")
+users = Blueprint("users", "backend")  # FIXME: don't hardcode backend
 
 users.add_url_rule("/login", "login", login, methods=["POST"])
 users.add_url_rule("/signup", "signup", signup, methods=["POST"])
